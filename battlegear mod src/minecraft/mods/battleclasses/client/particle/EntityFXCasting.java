255e29727078130f187123334c45c4240c90b821
Zsolt Molnár <zsolt.molnarrr@gmail.com>
2015-07-08 15:36:37 +0200

    Added basic entityFX class, updated gitignore.

---
+++ battlegear mod src/minecraft/mods/battleclasses/client/particle/EntityFXCasting.java
@@ -0,0 +1,25 @@
+package mods.battleclasses.client.particle;
+
+import net.minecraft.client.particle.EntityFX;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+public class EntityFXCasting extends EntityFX {
+
+	public EntityFXCasting(World p_i1219_1_, double p_i1219_2_,
+			double p_i1219_4_, double p_i1219_6_, double p_i1219_8_,
+			double p_i1219_10_, double p_i1219_12_) {
+		super(p_i1219_1_, p_i1219_2_, p_i1219_4_, p_i1219_6_, p_i1219_8_, p_i1219_10_,
+				p_i1219_12_);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	/*
+	public EntityFXCasting(World world, EntityPlayer entityPlayer) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	*/
+	
+}
